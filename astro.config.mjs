0199c16df34b44213d3ac79e3b65cab10d722c6d
J <80164315+jbolns@users.noreply.github.com>
2024-11-04 21:41:00 +0200

    GH Action Deploy #9

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ export default defineConfig({
 
         resolve: {
             alias: {
-                "~": "/src",
+                "~/assets": "../../../assets",
             },
         },
 
