06a19192b6254b2ad486d377dd319b4c2ed6102e
J <80164315+jbolns@users.noreply.github.com>
2026-03-28 14:49:07 +0200

    Visual improvements to animation wrapper and label renderer

--- src/components/AnimationWrapper.tsx
+++ src/components/AnimationWrapper.tsx
@@ -21,10 +21,10 @@ const AnimationWrapper = ({ locale }: any) => {
   if (animate === false) {
     return (
       <>
-        <div className="absolute top-0 -z-20 w-full overflow-hidden mx-auto bg-linear-to-r from-black via-yellow-300 to-teal-500 blur animate-pulse h-[calc(60vh+6px)] rounded-bl-[30vh] rounded-br-[100%]">
+        <div className="absolute top-0 -z-20 w-full overflow-hidden mx-auto bg-linear-to-r from-black via-yellow-300 to-teal-500 blur animate-pulse h-[calc(60vh+6px)] rounded-br-[100%]">
         </div>
 
-        <div className="flex w-full overflow-hidden mx-auto justify-center items-center bg-transparent dark:border-b dark:border-gray-300 h-[60vh] rounded-bl-[30vh] rounded-br-[100%]">
+        <div className="flex w-full overflow-hidden mx-auto justify-center items-center bg-transparent dark:border-b dark:border-gray-300 h-[60vh] rounded-br-[100%]">
           <div className="w-full text-center bg-transparent text-white">
             <button onClick={() => setAnimate(true)} className="border border-white border-dotted px-2 bg-gray-500 text-sm">{wrapperStrings.w1[locale]}</button>
             
@@ -32,7 +32,7 @@ const AnimationWrapper = ({ locale }: any) => {
         </div>
 
         <div id="simple"
-          className="absolute top-0 flex items-center justify-center -z-20 w-full overflow-hidden mx-auto bg-[#101f33] dark:bg-black h-[60vh] rounded-bl-[30vh] rounded-br-[100%]">
+          className="absolute top-0 flex items-center justify-center -z-20 w-full overflow-hidden mx-auto bg-[#101f33] dark:bg-black h-[60vh] rounded-br-[100%]">
           <span className="quad w-full h-[150vh] rounded-full animate-appear"></span>
           <span className="first w-[15vw] h-[15vw] animate-rotate bg-teal-300"></span>
         </div>
@@ -42,20 +42,20 @@ const AnimationWrapper = ({ locale }: any) => {
   } else {
     return (
       <>
-        <div className="absolute top-0 -z-10 w-full h-[calc(60vh+6px)] overflow-hidden mx-auto rounded-bl-[30vh] rounded-br-[100%] bg-linear-to-r from-black via-yellow-300 to-teal-500 blur animate-pulse">
+        <div className="absolute top-0 -z-10 w-full h-[calc(60vh+6px)] overflow-hidden mx-auto rounded-br-[100%] bg-linear-to-r from-black via-yellow-300 to-teal-500 blur animate-pulse">
         </div>
 
         <div id="simple"
-          className="absolute top-0 flex items-center justify-center -z-10 w-full overflow-hidden mx-auto h-[60vh] bg-[#101f33] dark:bg-black rounded-bl-[30vh] rounded-br-[100%]" >
+          className="absolute top-0 flex items-center justify-center -z-10 w-full overflow-hidden mx-auto h-[60vh] bg-[#101f33] dark:bg-black rounded-br-[100%]" >
           <span className="quad w-full h-[150vh] rounded-full animate-appear"></span>
         </div>
 
-        <div className="flex w-full overflow-hidden mx-auto h-[60vh] rounded-bl-[30vh] rounded-br-[100%] ">
+        <div className="flex w-full overflow-hidden mx-auto h-[60vh] rounded-br-[100%] ">
           <Animation client:load canvasWidth="100%" canvasHeight="100%" qual={qual} visualisation="bluemarble" />
         </div>
 
-        <p className="absolute mr-2 top-[6em] right-2 text-2sm text-white">
-          {[wrapperStrings.low, wrapperStrings.mid, wrapperStrings.high].map(q => { return (<button key={q[locale]} onClick={() => { resetQual(q[defaultLang]) }} className={"border border-dotted px-2 hover:bg-yellow-200 " + ((q[defaultLang] === qual) ? "active" : "")}>{q[locale].toUpperCase()} DEF</button>) })}
+        <p className="absolute top-14 right-7 text-white text-[0.7em]">
+          {[wrapperStrings.low, wrapperStrings.mid, wrapperStrings.high].map(q => { return (<button key={q[locale]} onClick={() => { resetQual(q[defaultLang]) }} className={"border border-dotted px-2 hover:bg-yellow-200" + ((q[defaultLang] === qual) ? "active" : "")}>{q[locale].toUpperCase()} DEF</button>) })}
         </p>
       </>
     )
